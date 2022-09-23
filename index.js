f6082f96de2a457b65f7710796b4aa0b26de0e1f
Daniel Miao <danielmiao2019@icloud.com>
2022-09-22 17:33:00 -0700

    started implementing authentication for board evaluation
    included a portion of the stockfish.js repository in the stockfish.js directory

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const crypto = require("crypto");
+var engine = require("./stockfish.js/load_engine")(__dirname + "/node_modules/stockfish/src/stockfish.js")
+
+engine.send("uci\n", function(data) {
+  console.log(data);
+});
 
 const app = express();
+
+var authorization_requests = {};
 
 app.use(bodyParser.text());
 
@@ -11,4 +19,42 @@ app.listen(3000, function() {
 
 app.get("/", function(request, response) {
   response.redirect("https://multiplayer-chess.gq/");
+});
+
+app.post("/auth", function(request, response) {
+  if (!request.headers["-x-uuid"]) {
+    response.status(400);
+    response.end();
+    console.log("hi");
+    return;
+  };
+  let data = crypto.randomInt(1000000) * parseInt(process.env.SECRET2);
+  authorization_requests[request.headers["-x-uuid"]] = data;
+  response.send(data.toString())
+});
+
+// auth request timeout and clear dictionary
+
+app.post("/result", function(request, response) {
+  if (!request.headers["-x-uuid"] || !authorization_requests[request.headers["-x-uuid"]]) {
+    response.status(400);
+    response.end();
+    return;
+  } else if (parseInt(request.body) == NaN || !request.headers["-x-fen"]) {
+    delete authorization_requests[request.headers["-x-uuid"]];
+    response.status(400);
+    response.end();
+    return;
+  } else if (parseInt(request.body) / parseInt(process.env.SECRET) * parseInt(process.env.SECRET2) != authorization_requests[request.headers["-x-uuid"]]) {
+    response.status(401);
+    response.end();
+    return;
+  };
+  response.send("authorized");
+  response.end();
+});
+
+app.post("/analyse", function(request, response) {
+  console.log(request.body)
+  response.send("a");
 });
